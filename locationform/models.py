0dfd6642278d0171bcfe4280f372073146b27294
Joyce <gathiithijoyce74@gmail.com>
2025-07-22 19:31:35 +0300

    modified models.py and added class meta in it

--- locationform/models.py
+++ locationform/models.py
@@ -7,9 +7,13 @@ class LocationRestrictedForm(models.Model):
     def __str__(self):
         return self.title
     
+    class Meta:
+        unique_together = ('form', 'email', 'submitted_at__date')
+    
 class AttendanceSubmission(models.Model):
     form=models.ForeignKey(LocationRestrictedForm, on_delete=models.CASCADE)  
     name= models.CharField(max_length=100)
+    location_accuracy = models.FloatField(null=True, blank=True)
     email = models.EmailField()
     present=models.BooleanField(default=False)
     Submitted_at = models.DateTimeField(auto_now_add=True)  
