bb700a079609ee6a0e4d4fb729400f5c3db9bd46
Joyce <gathiithijoyce74@gmail.com>
2025-07-22 18:56:10 +0300

    created in models.py class AttendanceSubmission

--- locationform/models.py
+++ locationform/models.py
@@ -7,4 +7,11 @@ class LocationRestrictedForm(models.Model):
     def __str__(self):
         return self.title
     
-    
+class AttendanceSubmission(models.Model):
+    form=models.ForeignKey(LocationRestrictedForm, on_delete=models.CASCADE)  
+    name= models.CharField(max_length=100)
+    email = models.EmailField()
+    present=models.BooleanField(default=False)
+    Submitted_at = models.DateTimeField(auto_now_add=True)  
+    def __str__(self):
+        return f"{self.name} - {self.form.title}"
