268537e8c10bc1ebb6d05b7bbb4745b60c85b939
Joyce <gathiithijoyce74@gmail.com>
2025-07-22 18:52:13 +0300

    creted in models.py class LocationRestrictedForm

--- locationform/models.py
+++ locationform/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
-# Create your models here.
+class LocationRestrictedForm(models.Model):
+    title =models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    def __str__(self):
+        return self.title
+    
+    
